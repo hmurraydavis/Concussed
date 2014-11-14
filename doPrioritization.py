b7c048a365b189fc3a5bd516d48a881243d7a2e0
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-11-14 00:46:32 -0500

    Support Vector Maciene works on fake data

--- doPrioritization.py
+++ doPrioritization.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pattern.en
 import messages
 import sklearn.linear_model 
+import sklearn.svm
 
 messages=messages.getMessages()
 
@@ -44,21 +45,36 @@ def getLengths():
 def makeTrainingVector():
     lengths = getLengths()
     tones = getTones()
+    subjectivities = getSubjectivity()
     trainingVector = zip(lengths, tones)
     return trainingVector
     
 def runAIFitting():
     trainingVector = makeTrainingVector()
     importances = getImportances()
+    C = 1.0  # SVM regularization parameter
     
-    clf = sklearn.linear_model.Ridge(alpha=1.0)
-    clf.fit(trainingVector,importances,sample_weight=None)
-    print clf.predict(trainingVector)
+#    clf = sklearn.linear_model.Ridge(alpha=1.0)
+    trainingVector = np.array(trainingVector)
+    print importances
+    rbf_svc = sklearn.svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(trainingVector, importances)
+#    print clf.predict(trainingVector)
     return clf
+    
+def runSVM():
+    from sklearn import svm
+    X = [[0, 0], [1, 1]]
+    y = [0, 1]
+    clf = svm.SVC()
+    clf.fit(X, y)  
+    svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, degree=3,
+    gamma=0.0, kernel='rbf', max_iter=-1, probability=False, random_state=None,
+    shrinking=True, tol=0.001, verbose=False)
 
 if __name__=='__main__':
     #print 'tone: ', getTones()
     #print 'import: ', getImportances()
     #print 'length is: ', getLengths()
     #print 'trainging vector: ', makeTrainingVector()
-    print 'ridge: ', runAIFitting()
+#    print 'ridge: ', runAIFitting()
+    runSVM()
