e1070cdc2b3a42252fec13fa191399d71fb0409f
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-12-01 22:06:09 -0500

    Gets unseen gmail emails

---
+++ getEmail.py
@@ -0,0 +1,20 @@
+import imaplib, re
+import os
+import time
+import socket
+
+imap_host = 'imap.gmail.com'
+mail = imaplib.IMAP4_SSL(imap_host)
+password = open('password.txt','r')
+password = password.read()
+mail.login("hmurraydavis@gmail.com", password)
+mail.select("inbox") # connect to inbox.
+
+while True:
+    try:
+        result, data = mail.uid('search', None, 'UNSEEN')
+        uid_list = data[0].split()
+        print len(uid_list), 'Unseen emails.'
+    except KeyboardInterrupt:
+        print 'Quitting'
+        pass
