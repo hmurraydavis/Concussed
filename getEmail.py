f23b70c7b2f1b770b7589a986f39ad4c99d303f5
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-12-01 22:09:18 -0500

    removes silly try except

--- getEmail.py
+++ getEmail.py
@@ -10,11 +10,7 @@ password = password.read()
 mail.login("hmurraydavis@gmail.com", password)
 mail.select("inbox") # connect to inbox.
 
-while True:
-    try:
-        result, data = mail.uid('search', None, 'UNSEEN')
-        uid_list = data[0].split()
-        print len(uid_list), 'Unseen emails.'
-    except KeyboardInterrupt:
-        print 'Quitting'
-        pass
+result, data = mail.uid('search', None, 'UNSEEN')
+uid_list = data[0].split()
+print len(uid_list), 'Unseen emails.'
+
