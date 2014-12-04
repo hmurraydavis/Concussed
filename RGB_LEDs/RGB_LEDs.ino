cde4caae7305c71bfe50d9cb000f11980d1e3cb4
Halie Murray-Davis <hmurraydavis@yahoo.com>
2014-12-04 02:14:59 -0500

    Starting to fix the serial reading in probalems

---
+++ RGB_LEDs/RGB_LEDs.ino
@@ -0,0 +1,121 @@
+#include <SoftwareSerial.h>
+
+#define GREEN 3
+#define BLUE 5
+#define RED 6
+#define delayTime 20
+
+char current_line[16]; // allocate some space for the string
+
+/* RGB LED code taken from a tutorial by cyragia:
+http://www.instructables.com/id/Fading-RGB-LED-Arduino/?ALLSTEPS 
+and modified by hmurraydavis */
+
+void setup() {
+
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(RED, HIGH);
+  
+  int redVal;
+  int blueVal;
+  int greenVal;
+}
+
+void read_line(char *line) {
+    // read characters from serial into line until a newline character
+    char c;
+    int index;
+    for (index = 0; index < 5; index++) {
+        // wait until there is a character
+        while (Serial.available() == 0);
+        // read a character
+        c = Serial.read();
+        if (c == '\n') {
+            break;
+        } else {
+            line[index] = c;
+        }
+    }
+    // terminate the string
+    line[index] = '\0';
+}
+
+int get_amount(char *line) {
+    // return the number in a string such as "r1200" as an int
+    int amount;
+    amount = (int) strtol(line+1, NULL, 10);
+    return amount;
+}
+
+
+int redV(serialString){
+    serialString = String(serialString);
+    redValue=serialString.substring(1,3);
+    Serial.println("red value is: " + String(redValue));
+    return redValue;
+}
+
+void writeColorToLED() {
+/*
+Writes color to RGB LEDs
+String format: 
+    n#rrrgggbbb where:
+        n is the character "n" denoting the start of a new string
+        # is the number of the desired chanel
+        rrr is three characters representing the value for the red chanel
+        ggg is three characters representing the value for the green chanel
+        bbb is three characters representing the value for the blue chanel
+*/
+
+}
+
+
+ 
+void loop() {
+
+    read_line(current_line);
+    if (current_line[0]=='n'){
+        redV(current_line);
+    }
+  /*
+  int redVal = 255;
+  int blueVal = 0;
+  int greenVal = 0;
+  for( int i = 0 ; i < 255 ; i += 1 ){
+    greenVal += 1;
+    redVal -= 1;
+    analogWrite( GREEN, 255 - greenVal );
+    analogWrite( RED, 255 - redVal );
+
+    delay( delayTime );
+  }
+ 
+  redVal = 0;
+  blueVal = 0;
+  greenVal = 255;
+  for( int i = 0 ; i < 255 ; i += 1 ){
+    blueVal += 1;
+    greenVal -= 1;
+    analogWrite( BLUE, 255 - blueVal );
+    analogWrite( GREEN, 255 - greenVal );
+
+    delay( delayTime );
+  }
+ 
+  redVal = 0;
+  blueVal = 255;
+  greenVal = 0;
+  for( int i = 0 ; i < 255 ; i += 1 ){
+    redVal += 1;
+    blueVal -= 1;
+    analogWrite( RED, 255 - redVal );
+    analogWrite( BLUE, 255 - blueVal );
+
+    delay( delayTime );
+  }
+  */
+}
